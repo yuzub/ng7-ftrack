067e2a30dc58e45eb41196e927a8cec294ff11a7
Yuriy Zubaha <yuriy.zubaha@gmail.com>
2018-12-26 15:33:53 +0200

    finished navigation

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -4,9 +4,13 @@ import {MatIconModule, MatIcon} from '@angular/material/icon';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatFormFieldModule, MatInputModule, MatSidenavModule],
-  exports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatFormFieldModule, MatInputModule, MatSidenavModule]
+  // tslint:disable-next-line:max-line-length
+  imports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatFormFieldModule, MatInputModule, MatSidenavModule, MatToolbarModule, MatListModule],
+  // tslint:disable-next-line:max-line-length
+  exports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatFormFieldModule, MatInputModule, MatSidenavModule, MatToolbarModule, MatListModule]
 })
 export class MaterialModule {}
